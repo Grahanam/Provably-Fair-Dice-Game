09acacc5809d40e5be1580be8573b1100dec856b
Grahanam <lunasuthar5221@gmail.com>
2025-03-04 22:43:04 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
+const port = 4000;
+const app = express();
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json());
+const outDir = path.join(__dirname, "out");
+
+app.use(express.static(outDir));
+
+function sha256(message) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(message);
+  return crypto.subtle.digest("SHA-256", data).then((hashBuffer) => {
+    return Array.from(new Uint8Array(hashBuffer))
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
+  });
+}
+
+app.post("/roll-dice", async (req, res) => {
+  try {
+    const clientSeed = req.body.clientSeed;
+    const serverSeed = "server-secret-code";
+    const nonce = Math.floor(Math.random() * 1000);
+    const combinedString = clientSeed + serverSeed + nonce;
+    const hash = await sha256(combinedString);
+
+    const diceRoll = (parseInt(hash.substring(0, 8), 16) % 6) + 1;
+    res.json({ diceValue: diceRoll });
+  } catch (error) {
+    res.json({ error: error });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on Port ${port}`);
+});
